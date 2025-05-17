f93488cd999746e0f778267111fa5293d6dbbbcb
monib-code <monibsiddiq999@gmail.com>
2025-05-17 07:22:54 -0700

    first commit

---
+++ app/Models/CompanyVoucherItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyVoucherItem extends Model
+{
+    use HasFactory;
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 10 + React 18</title>
+    @viteReactRefresh
+    @vite('resources/js/app.jsx')
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
